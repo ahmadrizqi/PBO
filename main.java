54c095e9c95497fd51b7c7ff95c53b0ffc67e6fe
ahmadrizqi <sket_acmilan76@yahoo.co.id>
2017-03-22 12:24:32 +0700

    Create main.java

---
+++ main.java
@@ -0,0 +1,24 @@
+# PBO
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package belajar;
+
+/**
+ *
+ * @author Rizqi
+ */
+public class Belajar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        form1 b = new form1();
+        b.setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
